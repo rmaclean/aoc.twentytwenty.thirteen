14e248995e0776f8d99aa3eabc52bc19b5199336
Robert MacLean <robert@sadev.co.za>
2020-12-14 15:13:42 +0200

    part 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var data = await File.ReadAllLinesAsync("data.txt");
+var earliestDepart = Convert.ToInt32(data[0]);
+var schedules = data[1].Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                    .Where(s => s != "x")
+                    .Select(s => Convert.ToInt32(s));
+
+var closestTime = earliestDepart + schedules.Max();
+var closestBus = 0;
+
+foreach (var schedule in schedules)
+{
+    for (var time = earliestDepart; time < closestTime; time++)
+    {
+        var onSchedule = time % schedule;
+        if (onSchedule == 0)
+        {
+            closestTime = time;
+            closestBus = schedule;
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"Closest Bus {closestBus} will be there at {closestTime}");
+Console.WriteLine($"Answer {(closestTime - earliestDepart) * closestBus}");
+
